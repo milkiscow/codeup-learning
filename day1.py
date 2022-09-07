c7d21ec8589dc56cf2b7143dbe9bb31b65526bf1
milkiscow <cheonjeonghwan04@gmail.com>
2022-09-07 11:32:06 +0900

    first commit

---
+++ day1.py
@@ -0,0 +1,23 @@
+#1
+print("Hello")
+
+#2
+print("Hello World")
+
+#3
+print("Hello\nWorld")
+
+#4
+print("'Hello'")
+
+#5
+print('"Hello World"')
+
+#6
+print("\"!@#$%^&*()\'")
+
+#7
+print("\"C:\Download\\'hello'.py\"")
+
+#8
+print("print(\"Hello\\nWorld\")")
