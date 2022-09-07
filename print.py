ed1ac9bfbfe10fe54ea0a891015d08aab02b3bb3
milkiscow <cheonjeonghwan04@gmail.com>
2022-09-07 14:20:15 +0900

    first day

---
+++ print.py
@@ -0,0 +1,49 @@
+# #1
+# print("Hello")
+
+# #2
+# print("Hello World")
+
+# #3
+# print("Hello\nWorld")
+
+# #4
+# print("'Hello'")
+
+# #5
+# print('"Hello World"')
+
+# #6
+# print("\"!@#$%^&*()\'")
+
+# #7
+# print("\"C:\Download\\'hello'.py\"")
+
+# #8
+# print("print(\"Hello\\nWorld\")")
+
+# #9
+# a = input()
+# b = input()
+# print(b)
+# print(a)
+
+# #10
+# f = float(input())
+# for i in range(3):
+#     print(f)
+    
+# #11
+# a,b = input().split()
+# a = int(a)
+# b = int(b)
+# print(a)
+# print(b)
+
+# #12
+# y,m,d = input().split('.')
+# print(d+'-'+m+'-'+y)
+
+#13
+a,b = input().split('-')
+print(a+b)
