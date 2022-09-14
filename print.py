d5076c23bb19127799f706578f4143266b4a927d
milkiscow <cheonjeonghwan04@gmail.com>
2022-09-14 11:11:36 +0900

    second day

--- print.py
+++ print.py
@@ -1,48 +1,48 @@
-# #1
-# print("Hello")
+#1
+print("Hello")
 
-# #2
-# print("Hello World")
+#2
+print("Hello World")
 
-# #3
-# print("Hello\nWorld")
+#3
+print("Hello\nWorld")
 
-# #4
-# print("'Hello'")
+#4
+print("'Hello'")
 
-# #5
-# print('"Hello World"')
+#5
+print('"Hello World"')
 
-# #6
-# print("\"!@#$%^&*()\'")
+#6
+print("\"!@#$%^&*()\'")
 
-# #7
-# print("\"C:\Download\\'hello'.py\"")
+#7
+print("\"C:\Download\\'hello'.py\"")
 
-# #8
-# print("print(\"Hello\\nWorld\")")
+#8
+print("print(\"Hello\\nWorld\")")
 
-# #9
-# a = input()
-# b = input()
-# print(b)
-# print(a)
+#9
+a = input()
+b = input()
+print(b)
+print(a)
 
-# #10
-# f = float(input())
-# for i in range(3):
-#     print(f)
+#10
+f = float(input())
+for i in range(3):
+    print(f)
     
-# #11
-# a,b = input().split()
-# a = int(a)
-# b = int(b)
-# print(a)
-# print(b)
+#11
+a,b = input().split()
+a = int(a)
+b = int(b)
+print(a)
+print(b)
 
-# #12
-# y,m,d = input().split('.')
-# print(d+'-'+m+'-'+y)
+#12
+y,m,d = input().split('.')
+print(d+'-'+m+'-'+y)
 
 #13
 a,b = input().split('-')
@@ -51,3 +51,4 @@ print(a+b)
 #14
 h,m,s = input().split(':')
 print(m)
+
